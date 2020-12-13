ddc3c8eb764da930696b0ca2da418ad47bd5bfa4
Alex Jiang <49266685+covertpluto3502@users.noreply.github.com>
2020-12-13 15:25:21 +0000

    Create iteration.py

---
+++ iteration.py
@@ -0,0 +1,30 @@
+# To do: learn to use for loops
+
+# For loops allow you to repeat an instruction for a SET AMOUNT of times
+
+# Example:
+
+# for i in range(10):
+#     print("Hello world")
+
+# The for keyword shows that this is a FOR loop
+# Ignore "i", "in" and "range" for now. All you need to know for now is how to use a FOR loop
+# Remember to indent any lines inside the for loop. This way the computer knows it is inside the loop.
+
+# Practice:
+
+# Can you make this loop print "hi" for 10 times?
+
+for i in range(5):
+    print("hi")
+    
+
+
+
+# hint: replace number 5
+
+# Can you create some code to print "hello" 100 times? Write it here:
+
+
+
+
