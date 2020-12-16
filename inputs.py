eeafb00d5180e7539a833f1400cc876a2a5b2a54
Alex Jiang <49266685+covertpluto3502@users.noreply.github.com>
2020-12-16 07:06:19 +0000

    Create inputs.py

---
+++ inputs.py
@@ -0,0 +1,23 @@
+# An INPUT is the computer taking data from the user
+
+# Example:
+input()
+
+# You can put the text you want to appear when the data is input
+# Example:
+input("How old are you?")
+
+# This asks the user that question
+# When an input is used, it should appear like this in the console:
+# >>>
+# How old are you?
+
+# There's a problem here: we can't use the data from the user
+# Thankfully, input() returns the user's input, for example:
+print(input("How old are you?"))
+
+# This will output whatever the user types
+
+# Can you make the program ask a question now? Try here:
+
+
