88fdc493500417385517bffb4b0e1e26a64e832d
Alex Jiang <49266685+covertpluto3502@users.noreply.github.com>
2020-12-28 08:17:19 +0000

    Create selection.py

---
+++ selection.py
@@ -0,0 +1,22 @@
+# How to use an 'if' statement
+# if *condition*:
+#     *code goes here*
+# If the condition is true, the code within runs
+
+# A true condition can be e.g 1 == 1 or 1 < 9
+# But 1 == 2 is NOT true
+
+# You should know a bit about operators...
+# Try making these print "Hello world", only changing the ?
+
+if ? == 8:
+    print("Hello world")
+    
+if ? >= 8:
+    print("Hello world")
+    
+if 2 ? 8:
+    print("Hello world")
+
+if ? != 3:
+    print("Hello world")
