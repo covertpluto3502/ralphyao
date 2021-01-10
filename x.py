2c4c4ae0fbb4b225115910ce9f45d75193b388fe
Alex Jiang <49266685+covertpluto3502@users.noreply.github.com>
2021-01-10 15:35:26 +0000

    Add files via upload

---
+++ x.py
@@ -0,0 +1,17 @@
+# Try to spot problems and fix
+# - typos
+# - syntax
+
+digits = [0, 1, 4, 2, 4, 6, 5, 5, 3]
+
+while true:
+    print("What item of the list do you want(or press Ctrl+C to exit)?)
+    user_input = imput()
+    index = int(user_input)
+    pront("Getting the item at", index)
+    print "Item at", index, "is", digits[index]
+
+    
+
+
+    
